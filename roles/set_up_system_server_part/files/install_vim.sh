7eab0d0360f86d9b6533f8c2f10c49b1de701808
Shuai Li <lishuai918@gmail.com>
2014-02-28 17:21:42 +0800

    Fix a bug installing vim.

--- roles/set_up_system_server_part/files/install_vim.sh
+++ roles/set_up_system_server_part/files/install_vim.sh
@@ -4,6 +4,6 @@ cd /tmp
 tar -xvf ./vim74.tar.gz
 cd vim74
 sudo make install
-sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin 40
+sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/vim 40
 cd ..
 rm -r vim74 vim74.tar.gz
