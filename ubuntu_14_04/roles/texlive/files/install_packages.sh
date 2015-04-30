9ebd2da90130773e6586741dcc20575aed3e05c6
Shuai Li <lishuai918@gmail.com>
2015-04-30 16:02:12 +0800

    Now citation related packages of texlive is added.

--- ubuntu_14_04/roles/texlive/files/install_packages.sh
+++ ubuntu_14_04/roles/texlive/files/install_packages.sh
@@ -8,7 +8,7 @@
 # Package-Requires: ()
 # Last-Updated:
 #           By:
-#     Update #: 17
+#     Update #: 19
 #
 #
 # Commentary:
@@ -30,6 +30,8 @@ tlmgr install framed fontawesome
 tlmgr install xecjk l3kernel l3packages
 # For moderncv
 tlmgr install moderncv xargs lipsum everypage
+# For beamer citation.
+tlmgr install biblatex logreq
 
 #
 # install_packages.sh ends here
