fedd1a28aee96b06a1f74824478ed5c374217812
Shuai Li <lishuai918@gmail.com>
2016-03-26 16:32:29 +0800

    Added another arg to init.sh to choose hosts. Separated pycuda from normall python packages. Typo and forgotten comments fixed. Added worker role to set up working env on servers.

--- ubuntu_14_04/roles/local_packages/files/py_pkgs.sh
+++ ubuntu_14_04/roles/local_packages/files/py_pkgs.sh
@@ -6,10 +6,3 @@ for pkg in pytest python-gflags tqdm ipython pyzmq tornado pyreadline jsonschema
 do
     $MINICONDA_BIN/pip install $pkg
 done
-
-# Install pycuda
-wget https://pypi.python.org/packages/source/p/pycuda/pycuda-2016.1.tar.gz -O /tmp/pycuda-2016.1.tar.gz
-tar -xvf /tmp/pycuda-2016.1.tar.gz -C /tmp
-cd /tmp/pycuda-2016.1
-$MINICONDA_BIN/python configure.py --cuda-root=/usr/local/cuda-7.5
-make install
