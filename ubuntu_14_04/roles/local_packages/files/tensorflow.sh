d797d3a21954fa854499497d34b86fa959465460
Shuai Li <lishuai918@gmail.com>
2016-08-01 17:33:41 +0800

    Upgrade tensorflow to 0.10 in local package role. Minor fix.

--- ubuntu_14_04/roles/local_packages/files/tensorflow.sh
+++ ubuntu_14_04/roles/local_packages/files/tensorflow.sh
@@ -3,6 +3,6 @@
 MINICONDA_BIN=$HOME/miniconda/bin
 
 # There is a bug in miniconda or pip. To get around we add --ignore-installed.
-$MINICONDA_BIN/pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.7.1-cp27-none-linux_x86_64.whl
+$MINICONDA_BIN/pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl
 
-echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64/"' >> $HOME/.bashrc
+echo 'export LD_LIBRARY_PATH="/usr/local/cuda/lib64/:$LD_LIBRARY_PATH"' >> $HOME/.bashrc
