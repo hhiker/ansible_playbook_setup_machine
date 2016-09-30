30582c5a6e864670a11b030b09c07d56bf10d732
Shuai Li <lishuai918@gmail.com>
2016-09-30 09:13:34 +0800

    Role local_packages:   Fixed wrong task description.   Fixed a pycuda bug.

--- ubuntu_14_04/roles/local_packages/files/pycuda.sh
+++ ubuntu_14_04/roles/local_packages/files/pycuda.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+MINICONDA_BIN=$HOME/miniconda/bin/
 
 # Install pycuda
 wget https://pypi.python.org/packages/source/p/pycuda/pycuda-2016.1.tar.gz -O /tmp/pycuda-2016.1.tar.gz
 tar -xvf /tmp/pycuda-2016.1.tar.gz -C /tmp
 cd /tmp/pycuda-2016.1
 $MINICONDA_BIN/python configure.py --cuda-root=/usr/local/cuda-7.5
-make install
+make -j 4 install
