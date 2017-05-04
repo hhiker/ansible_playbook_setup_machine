dea37b2707a7a565fa37902b6a46efdddcc6e563
Shuai Li <lishuai918@gmail.com>
2017-05-04 21:17:57 +0800

    Upgraded tensorflow and pycuda for role local_packages.

--- ubuntu_14_04/roles/local_packages/files/py_pkgs.sh
+++ ubuntu_14_04/roles/local_packages/files/py_pkgs.sh
@@ -2,7 +2,7 @@
 
 MINICONDA_BIN=$HOME/miniconda/bin
 
-for pkg in pytest python-gflags tqdm ipython pyzmq tornado pyreadline jsonschema decorator jinja2
+for pkg in pytest python-gflags tqdm ipython pyzmq tornado pyreadline jsonschema decorator jinja2 tensorflow
 do
     $MINICONDA_BIN/pip install $pkg
 done
--- ubuntu_14_04/roles/local_packages/files/pycuda.sh
+++ ubuntu_14_04/roles/local_packages/files/pycuda.sh
@@ -2,8 +2,8 @@
 MINICONDA_BIN=$HOME/miniconda/bin/
 
 # Install pycuda
-wget https://pypi.python.org/packages/source/p/pycuda/pycuda-2016.1.tar.gz -O /tmp/pycuda-2016.1.tar.gz
-tar -xvf /tmp/pycuda-2016.1.tar.gz -C /tmp
-cd /tmp/pycuda-2016.1
-$MINICONDA_BIN/python configure.py --cuda-root=/usr/local/cuda-7.5
+wget https://pypi.python.org/packages/e8/3d/4b6b622d8a22cace237abad661a85b289c6f0803ccfa3d2386103307713c/pycuda-2016.1.2.tar.gz -O /tmp/pycuda-2016.1.2.tar.gz
+tar -xvf /tmp/pycuda-2016.1.2.tar.gz -C /tmp
+cd /tmp/pycuda-2016.1.2
+$MINICONDA_BIN/python configure.py --cuda-root=/usr/local/cuda-8.0
 make -j 4 install
