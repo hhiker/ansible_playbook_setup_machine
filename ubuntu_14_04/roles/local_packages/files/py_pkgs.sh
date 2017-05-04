df97ee0255544a15f47e81457be1f71e63c931ab
Shuai Li <lishuai918@gmail.com>
2017-05-04 22:09:14 +0800

    Changed local_packages.

--- ubuntu_14_04/roles/local_packages/files/py_pkgs.sh
+++ ubuntu_14_04/roles/local_packages/files/py_pkgs.sh
@@ -2,7 +2,7 @@
 
 MINICONDA_BIN=$HOME/miniconda/bin
 
-for pkg in pytest python-gflags tqdm ipython pyzmq tornado pyreadline jsonschema decorator jinja2 tensorflow
+for pkg in pytest python-gflags tqdm ipython pyzmq tornado pyreadline jsonschema decorator jinja2 tensorflow_gpu
 do
     $MINICONDA_BIN/pip install $pkg
 done
