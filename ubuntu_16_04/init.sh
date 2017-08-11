f865f873b8ce3fa522e0fadcdba3d874fae66757
Shuai Li <lishuai918@gmail.com>
2017-08-11 15:08:26 +0800

    Added playbook for Ubuntu 16.04.

---
+++ ubuntu_16_04/init.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+usage()
+{
+    printf "%b" "
+Usage:
+
+Set up Ansible on Control Machine.
+
+The OS should be Ubuntu 14.04.
+
+bash init.sh USER_NAME HOSTS
+
+Args:
+    USER_NAME: You user name on the control machine you would like to use
+    HOSTS: hosts to run on
+        Ansible with.
+
+Boilerplates are commented out in the script. Uncomment them to use.
+"
+}
+
+if [ $# -ne 2 ]
+then
+    usage
+    exit 0
+fi
+
+USER_NAME=$1
+HOSTS=$2
+
+# Clean up home folder.
+cd
+rmdir Desktop Documents Music Pictures Public Templates Videos
+
+# Install necessary softwares.
+sudo apt-get -y install tmux git vim
+# Better output format for Ansible.
+sudo apt-get -y install cowsay
+# We have to use password to sometimes.
+sudo apt-get -y install sshpass
+sudo apt-get -y install python-pip
+sudo pip install paramiko PyYAML jinja2 httplib2 six
+sudo apt-get -y install openssh-server
+
+# Varaibles.
+ROOT_FOLDER="$HOME/mine"
+SOFTWARE_FOLDER="${ROOT_FOLDER}/softwares"
+
+# # Setup directory structure.
+mkdir -p ${SOFTWARE_FOLDER}
+
+# # Setup ansible
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
+
+# # Setup ssh.
+cd
+echo -e 'y\n' |  ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+ssh-add
+# TODO(Shuai) Change known_host directly instead ssh once.
+ssh 127.0.0.1
+
+PLAYBOOK_HOME="${SOFTWARE_FOLDER}/playbooks/ubuntu_16_04"
+
+# Run playbooks.
+# ansible-playbook "${PLAYBOOK_HOME}/site.yml" -vvvv\
+#     -i "${PLAYBOOK_HOME}/hosts" -K \
+#     --extra-vars "hosts=local_machine user=$USER_NAME"
+
+# # Install octopress.
+# Use this manually.
+# ansible-playbook "${PLAYBOOK_HOME}/octopress.yml" \
+    # -i "${PLAYBOOK_HOME}/hosts" -K \
+    # --extra-vars "hosts=local_machine user=$USER_NAME"
+
+# # Install emacs.
+# ansible-playbook "${PLAYBOOK_HOME}/emacs.yml" \
+#     -i "${PLAYBOOK_HOME}/hosts" -K \
+#     --extra-vars "hosts=local_machine user=$USER_NAME"
+
+# # Install R.
+# ansible-playbook "${PLAYBOOK_HOME}/R.yml" \
+#     -i "${PLAYBOOK_HOME}/hosts" -K \
+#     --extra-vars "hosts=local_machine user=$USER_NAME"
+
+# # Install caffe.
+# # playbook for caffe is commented in case of accidentally install it since
+# # normally it only will be installed on powerful pc or server.
+# ansible-playbook "${PLAYBOOK_HOME}/caffe.yml" \
+#     -i "${PLAYBOOK_HOME}/hosts" -K \
+#     --extra-vars "hosts=local_machine user=$USER_NAME"
+
+# # Install texlive.
+# ansible-playbook "${PLAYBOOK_HOME}/texlive.yml" \
+#     -i "${PLAYBOOK_HOME}/hosts" -K \
+#     --extra-vars "hosts=local_machine user=$USER_NAME"
+
+# # Install blocks and fuel.
+# ansible-playbook "${PLAYBOOK_HOME}/blocks_fuel.yml" \
+    # -i "${PLAYBOOK_HOME}/hosts" -K \
+    # --extra-vars "hosts=local_machine user=$USER_NAME"
+
+# ansible-playbook "${PLAYBOOK_HOME}/test.yml" \
+    # -i "${PLAYBOOK_HOME}/hosts" -K \
+    # --extra-vars "hosts=local_machine user=$USER_NAME"
+
+# ansible-playbook "${PLAYBOOK_HOME}/desktop.yml" \
+    # -i "${PLAYBOOK_HOME}/hosts" -K \
+    # --extra-vars "hosts=local_machine user=$USER_NAME"
+
+# ansible-playbook "${PLAYBOOK_HOME}/setup_user_home.yml" \
+#     -i "${PLAYBOOK_HOME}/hosts" -k \
+#     --extra-vars "hosts=$HOSTS user=$USER_NAME new_user=$USER_NAME"
+
+# ansible-playbook "${PLAYBOOK_HOME}/worker.yml" \
+#     -i "${PLAYBOOK_HOME}/hosts" -K \
+#     --extra-vars "hosts=$HOSTS user=$USER_NAME"
+
+# ansible-playbook "${PLAYBOOK_HOME}/tensorflow_0.6.yml" \
+    # -i "${PLAYBOOK_HOME}/hosts" -K \
+    # --extra-vars "hosts=ie_machine user=$USER_NAME"
+
+# ansible-playbook "${PLAYBOOK_HOME}/local_packages.yml" \
+#     -i "${PLAYBOOK_HOME}/hosts" \
+#     --extra-vars "hosts=$HOSTS user=$USER_NAME"
+
+# ansible-playbook "${PLAYBOOK_HOME}/local_tf_0.6.yml" \
+#     -i "${PLAYBOOK_HOME}/hosts" \
+#     --extra-vars "hosts=$HOSTS user=$USER_NAME"
+
+
+# ansible-playbook "${PLAYBOOK_HOME}/programming.yml" \
+    # -i "${PLAYBOOK_HOME}/hosts" -K \
+    # --extra-vars "hosts=local_machine user=$USER_NAME"
---
+++ ubuntu_16_04/roles/texlive/files/install_packages.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Copyright 2015
+#
+# Filename: install_packages.sh
+# Author: Shuai
+# Contact: lishuai918@gmail.com
+# Created: Tue Mar 31 15:26:27 2015 (+0800)
+# Package-Requires: ()
+# Last-Updated:
+#           By:
+#     Update #: 32
+#
+#
+# Commentary:
+# This script is used to install necessary tex package after installing texlive
+# from ubuntu source.
+#
+#
+#
+# Code:
+
+
+
+tlmgr init-usertree
+# Let tlmgr download documentation when installing new packages.
+tlmgr option docfiles 1
+
+# For beamer
+# #########################################################################
+
+# For mdframed.
+tlmgr install mdframed etoolbox needspace
+# For fancy title page.
+tlmgr install framed fontawesome
+# For Chinese.
+tlmgr install xecjk l3kernel l3packages
+# For moderncv
+tlmgr install moderncv xargs lipsum everypage
+# For beamer citation.
+tlmgr install biblatex logreq
+# For floating comments.
+tlmgr install textpos
+# For including files from different folders.
+tlmgr install import
+
+# For general latex.
+# #########################################################################
+# Though those two packages have been included by default, it seems to remove
+# some bugs to install it again. Maybe the new version is installed.
+tlmgr install fontspec xltxtra
+tlmgr install ifplatform xstring lineno paralist cleveref
+# Footnote in a table
+tlmgr install tablefootnote ifmtarg xifthen
+
+# For math typeset.
+# #########################################################################
+tlmgr install commath
+
+# NIPS template dependency
+# #######################################################################
+tlmgr install microtype units
+
+# For writing drafts
+# #######################################################################
+tlmgr install todonotes
+
+# For coding
+# #######################################################################
+tlmgr install minted
+
+
+#
+# install_packages.sh ends here
